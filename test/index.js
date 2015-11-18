09930315def07f3b5bec93e35719f574e1b11023
Chris Morris <morriswchris@gmail.com>
2015-11-17 19:29:10 -0500

    fixed linting for test

--- test/index.js
+++ test/index.js
@@ -1,80 +1,85 @@
-var superagent = require('superagent')
-var expect = require('expect.js')
+var superagent = require("superagent");
+var expect = require("expect.js");
 
-describe('express rest api server', function(){
-  var id
+describe("express rest api server", function() {
+  var id;
 
-  it('posts an object', function(done){
-    superagent.post('http://localhost:3000/test')
-      .send({ name: 'John'
-        , email: 'john@rpjs.co'
+  it("posts an object", function(done) {
+    superagent.post("http://localhost:3000/test")
+      .send({
+        name: "John",
+        email: "john@rpjs.co"
       })
-      .end(function(e, res){
+      .end(function(e, res) {
         // console.log(res.body)
-        expect(e).to.eql(null)
-        expect(res.body._id.length).to.eql(24)
+        expect(e).to.eql(null);
+        expect(res.body._id.length).to.eql(24);
         id = res.body._id;
-        done()
-      })
-  })
+        done();
+      });
+  });
 
-  it('retrieves an object', function(done){
-    superagent.get('http://localhost:3000/test/'+id)
-      .end(function(e, res){
+  it("retrieves an object", function(done) {
+    superagent.get("http://localhost:3000/test/" + id)
+      .end(function(e, res) {
         // console.log(res.body)
-        expect(e).to.eql(null)
-        expect(typeof res.body).to.eql('object')
-        expect(res.body._id.length).to.eql(24)
-        expect(res.body._id).to.eql(id)
-        expect(res.body.name).to.eql('John')
-        done()
-      })
-  })
+        expect(e).to.eql(null);
+        expect(typeof res.body).to.eql("object");
+        expect(res.body._id.length).to.eql(24);
+        expect(res.body._id).to.eql(id);
+        expect(res.body.name).to.eql("John");
+        done();
+      });
+  });
 
-  it('retrieves a collection', function(done){
-    superagent.get('http://localhost:3000/test')
-      .end(function(e, res){
+  it("retrieves a collection", function(done) {
+    superagent.get("http://localhost:3000/test")
+      .end(function(e, res) {
         // console.log(res.body)
-        expect(e).to.eql(null)
-        expect(res.body.length).to.be.above(0)
-        expect(res.body.map(function (item){return item._id})).to.contain(id)
-        done()
-      })
-  })
+        expect(e).to.eql(null);
+        expect(res.body.length).to.be.above(0);
+        expect(res.body.map(function(item) {
+          return item._id;
+        })).to.contain(id);
+        done();
+      });
+  });
 
-  it('updates an object', function(done){
-    superagent.put('http://localhost:3000/test/'+id)
-      .send({name: 'Peter'
-        , email: 'peter@yahoo.com'})
-      .end(function(e, res){
-        // console.log(res.body)
-        expect(e).to.eql(null)
-        expect(typeof res.body).to.eql('object')
-        expect(res.body.msg).to.eql('success')
-        done()
+  it("updates an object", function(done) {
+    superagent.put("http://localhost:3000/test/" + id)
+      .send({
+        name: "Peter",
+        email: "peter@yahoo.com"
       })
-  })
+      .end(function(e, res) {
+        // console.log(res.body)
+        expect(e).to.eql(null);
+        expect(typeof res.body).to.eql("object");
+        expect(res.body.msg).to.eql("success");
+        done();
+      });
+  });
 
-  it('checks an updated object', function(done){
-    superagent.get('http://localhost:3000/test/'+id)
-      .end(function(e, res){
+  it("checks an updated object", function(done) {
+    superagent.get("http://localhost:3000/test/" + id)
+      .end(function(e, res) {
         // console.log(res.body)
-        expect(e).to.eql(null)
-        expect(typeof res.body).to.eql('object')
-        expect(res.body._id.length).to.eql(24)
-        expect(res.body._id).to.eql(id)
-        expect(res.body.name).to.eql('Peter')
-        done()
-      })
-  })
-  it('removes an object', function(done){
-    superagent.del('http://localhost:3000/test/'+id)
-      .end(function(e, res){
+        expect(e).to.eql(null);
+        expect(typeof res.body).to.eql("object");
+        expect(res.body._id.length).to.eql(24);
+        expect(res.body._id).to.eql(id);
+        expect(res.body.name).to.eql("Peter");
+        done();
+      });
+  });
+  it("removes an object", function(done) {
+    superagent.del("http://localhost:3000/test/" + id)
+      .end(function(e, res) {
         // console.log(res.body)
-        expect(e).to.eql(null)
-        expect(typeof res.body).to.eql('object')
-        expect(res.body.msg).to.eql('success')
-        done()
-      })
-  })
-})
+        expect(e).to.eql(null);
+        expect(typeof res.body).to.eql("object");
+        expect(res.body.msg).to.eql("success");
+        done();
+      });
+  });
+});
